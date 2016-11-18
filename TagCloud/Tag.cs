86a103f4f9f306cab6e06e7a7325ae1bb9d10b82
madsdj <madsdj@gmail.com>
2016-11-18 23:23:07 +0100

    Fixed Name = null

--- TagCloud/Tag.cs
+++ TagCloud/Tag.cs
@@ -18,7 +18,7 @@ namespace TagCloud
         public string Name // brug ikke dette som key! -- hvad hvis jeg vil ændre værdien?
         {
             get { return _name; }
-            set { _name = value.ToLower(); }
+            set { _name = value?.ToLower(); }
         }
         public virtual ICollection<Pizza> Pizzas { get; set; }
     }
